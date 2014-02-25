cfbc4e195d35182d76c714e3dcd0e942444e3918
wenren <xiangwenwe@foxmail.com>
2014-02-25 10:59:37 +0800

    douban help shell

---
+++ src/douban_help.py
@@ -0,0 +1,11 @@
+__author__ = 'xiangwenwen'
+
+def dbhelp(client):
+    print('douban help : view douban help doc')
+    print('show me : view your information')
+    print('book : enter book shell')
+    print('-------   search [string search book name|tag|start|count] : view search book on douban.com')
+    print('-------   commit [string info] : commit -m to douban.com')
+    print('-------   rm [string info] : remove your comment to douban.com')
+    print('-------   up [string info] : update your comment to douban.com')
+    pass
