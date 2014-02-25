128c73f5cd17fba3f91cca8f508a81eac7597506
wenren <xiangwenwe@foxmail.com>
2014-02-25 15:52:17 +0800

    update main

--- main.py
+++ main.py
@@ -13,16 +13,16 @@ if client == 'error':
     print('error oauth')
 else:
     while True:
-           shell = douban_controlCenter.center()
-           if shell == 'exit':
-                exit()
-                pass
-           try:
-               douban_controlCenter.shellDic[shell](client)
-               pass
-           except KeyError:
-                print(shell +  ' is not a douban shell command see douban help')
-                continue      
-           except  KeyboardInterrupt:
-                exit()     
+       shell = douban_controlCenter.center()
+       if shell == 'exit':
+            exit()
+            pass
+       try:
+           douban_controlCenter.shellDic[shell](client)
+           pass
+       except KeyError:
+            print(shell +  ' is not a douban shell command see douban help')
+            continue
+       except  KeyboardInterrupt:
+            exit()
     pass
