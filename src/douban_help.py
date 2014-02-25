49aec21f7b083d3143a807f4b5fc974d021a9ff2
wenren <xiangwenwe@foxmail.com>
2014-02-25 15:45:02 +0800

    update help information

--- src/douban_help.py
+++ src/douban_help.py
@@ -3,9 +3,14 @@ __author__ = 'xiangwenwen'
 def dbhelp(client):
     print('douban help : view douban help doc')
     print('show me : view your information')
+    print('show : enter search user shell')
+    print('-------   search [string search user] : view you need search user on douban.com')
+    print('-------   show [id] : view your information ')
     print('book : enter book shell')
     print('-------   search [string search book name|tag|start|count] : view search book on douban.com')
     print('-------   commit [string info] : commit -m to douban.com')
     print('-------   rm [string info] : remove your comment to douban.com')
     print('-------   up [string info] : update your comment to douban.com')
+    print('-------   show [string info] : view book information')
+    print('-------   tags [string id] : view book tag')
     pass
