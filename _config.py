2684e3e3a7a8208d6693f982702094312b5edca1
wenren <xiangwenwe@foxmail.com>
2014-02-25 17:39:02 +0800

    commit config py

---
+++ _config.py
@@ -0,0 +1,9 @@
+__author__ = 'xiangwenwen'
+
+def configKey():
+     return {
+          'API_KEY':'',
+          'API_SECRET':'',
+          'CALLBACK_URL':'',
+          'SCOPE':'douban_basic_common,shuo_basic_r,shuo_basic_w,book_basic_r,book_basic_w'
+     } 
