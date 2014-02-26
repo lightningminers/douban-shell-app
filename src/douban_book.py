4ca0e8e0b16d19ad45f119fcf5bcfd12d15b1a60
wenren <xiangwenwe@foxmail.com>
2014-02-26 11:14:59 +0800

    add error try outinput file and view review id

--- src/douban_book.py
+++ src/douban_book.py
@@ -1,9 +1,12 @@
 __author__ = 'xiangwenwen'
 import douban_client.api
 import math
+import traceback
 import os
 import sqlite3
-dbPath = os.getcwd() + os.path.sep+'db'+os.path.sep + 'doubanShellApp.db'
+work_path = os.getcwd() + os.path.sep
+dbPath = work_path + 'db'+os.path.sep + 'doubanShellApp.db'
+errorPath = work_path + 'error' + os.path.sep + 'Douban_API_ERROR.txt'
 class Book_search:
     def __init__(self):
       self.bookDic = {
@@ -29,26 +32,33 @@ class Book_search:
                 clientValue = shell[1]
                 self.bookDic[sh](client,clientValue)
             except IndexError:
+                self.error_outinput()
                 print(sh + ' your commit message is empty')
         else:
             print(sh + ' is not a douban shell command see douban help')
+    #搜索书籍
     def book_search(self,client,val):
        print('HTTP douban data wait ....')
        val = val.split('|')
        q = val[0]
        start = val[1]
        count = val[2]
-       searchDate = client.book.search(q,start=start,count=count)
-       print('\n')
-       for node in searchDate['books']:
-            print('book id : ' + node['id'])
-            print('book name : ' + node['title'])
-            print('book author : ' + node['author'][0])
-            print('book publisher : ' + node['publisher'])
-            print('book time : ' + node['pubdate'])
+       try:
+            searchDate = client.book.search(q,start=start,count=count)
             print('\n')
-       print('message : select id commit up rm show tags')
+            for node in searchDate['books']:
+                print('book id : ' + node['id'])
+                print('book name : ' + node['title'])
+                print('book author : ' + node['author'][0])
+                print('book publisher : ' + node['publisher'])
+                print('book time : ' + node['pubdate'])
+                print('\n')
+            print('message : select id commit up rm show tags')
+       except douban_client.api.error.DoubanAPIError:
+            self.error_outinput()
+            print('message : search book error')
        pass
+    #给书籍评论
     def book_commit(self,client,val):
       print('HTTP douban data wait ....')
       val = val.split('|')
@@ -56,19 +66,20 @@ class Book_search:
       booktitle = val[1]
       bookcontent = val[2]
       try:
-          reviewsDate = client.book.review.new(bookid,booktitle,bookcontent)
-          reviewsid =  reviewsDate['id']
-          CX = sqlite3.connect(dbPath)
-          CU= CX.cursor()
-          CU.execute('insert into bookreview values (reviewid,bookid)',(reviewsid,bookid))
-          CX.close()
+            reviewsDate = client.book.review.new(bookid,booktitle,bookcontent)
+            reviewsid =  reviewsDate['id']
+            CX = sqlite3.connect(dbPath)
+            CU= CX.cursor()
+            CU.execute('insert into bookreview values (reviewid,bookid)',(reviewsid,bookid))
+            CX.close()
       except douban_client.api.error.DoubanAPIError:
-          print(douban_client.api.error.DoubanAPIError)
-          print('need more than 150 words')
+            self.error_outinput()
+            print('message : commit book error need 150 ')
+    #查看某本书籍的详细信息
     def book_show(self,client,val):
+        print('HTTP douban data wait ....')
+        print('\n')
         try:
-            print('HTTP douban data wait ....')
-            print('\n')
             showDate = client.book.get(val)
             print('book name : ' + showDate['title'])
             print('book press : ' + showDate['publisher'])
@@ -87,8 +98,9 @@ class Book_search:
             # print('book summary : ' + summary)
             print('\n')
         except douban_client.api.error.DoubanAPIError:
-            print(douban_client.api.error.DoubanAPIError)
-            print('book not found')
+            self.error_outinput()
+            print('message : show book error')
+    #更新书籍的某条评论
     def book_update(self,client,val):
         print('HTTP douban update reviews wait ....')
         val = val.split('|')
@@ -99,14 +111,18 @@ class Book_search:
             client.book.review.update(reviewsid,newtitle,newcontent)
             print('message : update success')
         except douban_client.api.error.DoubanAPIError:
+            self.error_outinput()
             print('message : HTTP error')
+    #删除书籍的某条评论
     def book_remove(self,client,val):
         print('HTTP douban remove reviews wait ....')
         try:
             client.book.review.delete(val)
             print('message : remove reviews success')
         except douban_client.api.error.DoubanAPIError:
+            self.error_outinput()
             print('message : HTTP error')
+    #查看书籍的标签
     def book_tags(self,client,val):
        try:
           tagsData = client.book.tags(val)
@@ -116,8 +132,9 @@ class Book_search:
                print('tag : ' + node['title'])
           print('\n')
        except douban_client.api.error.DoubanAPIError:
-          print(douban_client.api.error.DoubanAPIError)
+          self.error_outinput()
           print('value error or HTTP error')
+    #查看评论书籍之后的reviews id
     def book_reviews_show(self,val):
         bookid = val
         CX = sqlite3.connect(dbPath)
@@ -127,6 +144,12 @@ class Book_search:
         print('view bookid as reviewsid' + bookid)
         print(reviewID)
         CX.close()
+    #错误信息输出
+    def error_outinput(self):
+        error_f = open(errorPath,'a')
+        traceback.print_exc(file=error_f)
+        error_f.close()
+        pass
 book = Book_search()
 def bookStatr(client):
   book_bool = True
