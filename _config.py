6cbac9152c23a8ff131e981f0612c4943d116459
wenren <xiangwenwe@foxmail.com>
2014-03-01 15:22:21 +0800

    add douban music

--- _config.py
+++ _config.py
@@ -5,5 +5,5 @@ def configKey():
           'API_KEY':'',
           'API_SECRET':'',
           'CALLBACK_URL':'',
-          'SCOPE':'douban_basic_common,shuo_basic_r,shuo_basic_w,book_basic_r,book_basic_w'
+          'SCOPE':'douban_basic_common,shuo_basic_r,shuo_basic_w,book_basic_r,book_basic_w,music_basic_r,music_basic_w,music_artist_r'
      } 
